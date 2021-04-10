6c5fe5e41353c5f3e7d33d7cef2cf23461a08357
zouhir <zouhirtaousy@gmail.com>
2021-04-10 18:51:38 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,144 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import { useEffect } from 'react';
+
+import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Alert
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+
+function readNdef() {
+  const cleanUp = () => {
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+    NfcManager.setEventListener(NfcEvents.SessionClosed, null);
+  };
+
+  return new Promise((resolve) => {
+    let tagFound = null;
+
+    NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+      tagFound = tag;
+      resolve(tagFound);
+      NfcManager.setAlertMessageIOS('NDEF tag found');
+      NfcManager.unregisterTagEvent().catch(() => 0);
+    });
+
+    NfcManager.setEventListener(NfcEvents.SessionClosed, () => {
+      cleanUp();
+      if (!tagFound) {
+        resolve();
+      }
+    });
+
+    NfcManager.registerTagEvent();
+  });
+}
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    NfcManager.start().then(res=>{
+      console.log(res);
+      readNdef().then(data=>{
+        console.log(data);
+        Alert.alert(data);
+      });
+    },err=>{
+      console.log(err)
+    })
+  }, [])
+  
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="NFC">
+            NFC TESTER
+          </Section>
+          
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
